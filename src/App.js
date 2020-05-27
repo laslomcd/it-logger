acdbc41307a23fc3e17cde4a536bbcde0cae4c37
ryan <laslomcd@gmail.com>
2020-05-27 14:40:43 -0400

    finished UI setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, Fragment } from "react";
+import SearchBar from "./components/Navbar/SearchBar";
+import Logs from "./components/Logs/Logs";
+import AddBtn from "./components/Buttons/AddBtn";
+import AddLogModal from "./components/Modals/AddLogModal";
+import EditLogModal from "./components/Modals/EditLogModal";
+import AddTechModal from "./components/Modals/AddTechModal";
+import ShowTechsModal from "./components/Modals/ShowTechsModal";
 
-function App() {
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import "./App.css";
+
+const App = () => {
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <ShowTechsModal />
+        <Logs />
+      </div>
+    </Fragment>
   );
-}
-
+};
 export default App;
